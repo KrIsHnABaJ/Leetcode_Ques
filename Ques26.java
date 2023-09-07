86c9ffad3ad3495a08e760441bddb736dae89407
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-09-08 00:51:44 +0530

    26. Remove Duplicates from Sorted Array
    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
    
    Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
    
    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
    Return k.

--- Ques26.java
+++ Ques26.java
@@ -2,17 +2,17 @@ import java.util.Arrays;
 
 public class Ques26 {
     public static void main(String[] args) {
-        int[] arr ={0,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,};
+        int[] arr ={#anyyyy};
         System.out.println(removeDuplicates(arr));
     }
     public static int removeDuplicates(int[] nums) {
-        Arrays.sort(nums);
-        int count =1;
-        for (int i = 0; i < nums.length-1; i++) {
-            if(nums[i]!=nums[i+1]){
-                count++;
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        return count;
+        return j;
     }
 }
