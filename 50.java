0a7bc090d7736287e1b882470c17e67b171fbb68
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-09-10 01:40:56 +0530

    50. Pow(x, n)
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).

---
+++ 50.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(myPow(#any double,int));
+    } 
+    public double myPow(double x, int n) {
+       return Math.pow(x,n);
+    }
+}
