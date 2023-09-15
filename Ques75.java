6ed6144bb5382d970f642386c1c2aa8e0ba17a33
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-09-16 00:47:12 +0530

    75. Sort Colors
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
    
    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.

---
+++ Ques75.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String[] args) {
+        int[] arr ={#any num};
+        system.out.println(sortColors(arr));
+    }
+    public static void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+}
