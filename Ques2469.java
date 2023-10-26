9cbe6de1780d82d25519ae0690c97a90d4ef046f
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-10-27 01:53:13 +0530

    2469. Convert the Temperature
    You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
    
    You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
    
    Return the array ans. Answers within 10-5 of the actual answer will be accepted.
    
    Note that:
    
    Kelvin = Celsius + 273.15
    Fahrenheit = Celsius * 1.80 + 32.00

---
+++ Ques2469.java
@@ -0,0 +1,11 @@
+class Ques2469 {
+    public static void main (String[] args){
+        System.out.println(#any double);
+    }  
+    public static double[] convertTemperature(double celsius) {
+        double[] res = new double[2];
+        res[0] = celsius + 273.15;
+        res[1] = celsius * 1.80 + 32.00;       
+        return res;
+    }
+}
