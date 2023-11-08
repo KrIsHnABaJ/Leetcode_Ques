d92396ad6be65f2851a779a3304e38fbaf733c4d
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-11-09 01:18:40 +0530

    2119. A Number After a Double Reversal
    Reversing an integer means to reverse all its digits.
    
    For example, reversing 2021 gives 1202. Reversing 12300 gives 321 as the leading zeros are not retained.
    Given an integer num, reverse num to get reversed1, then reverse reversed1 to get reversed2. Return true if reversed2 equals num. Otherwise return false.

---
+++ Ques2119.java
@@ -0,0 +1,15 @@
+class  Ques2119{
+    public static void main(String[], args){
+        System.out.println(isSameAfterReversals(#int));
+  }  
+    public static boolean isSameAfterReversals(int num) {
+        if(num==0){
+            return true;
+        }
+        if(num%10==0){
+            return false;
+        }
+        return true;
+        
+    }
+}
