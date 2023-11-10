331893e6ce4d8f01e5a2f41471ec662a22b0f783
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-11-11 01:12:41 +0530

    405. Convert a Number to Hexadecimal
    Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
    
    All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
    
    Note: You are not allowed to use any built-in library method to directly solve this problem.

---
+++ Ques405.java
@@ -0,0 +1,11 @@
+class Ques405 {
+        public static void main(String[], args){
+          Systen.out.println(toHex(#int));
+        }
+
+       public static String toHex(int num) {
+        String hex = Integer.toHexString(num);
+        return hex;
+        
+    }
+}
