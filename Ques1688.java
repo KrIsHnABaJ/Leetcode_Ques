4ba19448dc58c0505fd3338261ad53b849e5260f
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-12-06 00:07:27 +0530

    1688. Count of Matches in Tournament
    You are given an integer n, the number of teams in a tournament that has strange rules:
    
    If the current number of teams is even, each team gets paired with another team. A total of n / 2 matches are played, and n / 2 teams advance to the next round.
    If the current number of teams is odd, one team randomly advances in the tournament, and the rest gets paired. A total of (n - 1) / 2 matches are played, and (n - 1) / 2 + 1 teams advance to the next round.
    Return the number of matches played in the tournament until a winner is decided.

---
+++ Ques1688.java
@@ -0,0 +1,8 @@
+public class Ques1688 {
+    public static void main(String[] args) {
+        System.out.println(numberOfMatches(7));
+    }
+    public static int numberOfMatches(int n) {
+        return n-1;
+    }
+}
